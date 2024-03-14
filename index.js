1e7620ee736c41b44e6e309b48fd11ca339c8eed
cl4ram <molinayvediaclara@outlook.com>
2024-03-14 15:47:02 -0300

    update(*): feedback and accesibility issues

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-const acc = document.getElementsByClassName("label-container");
+const disclosures = document.querySelectorAll('.js-disclosure');
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+function toggleDisclosure() {
+  const isExpanded = this.getAttribute('aria-expanded') === 'true';
+  this.setAttribute('aria-expanded', !isExpanded);
+  const element = this.lastElementChild
 
-    const panel = this.nextElementSibling;
-    const isItemActive = this.classList.contains("active");
-    const element = this.lastElementChild
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-    
-
-    if (isItemActive) {
-        element.src = "./assets/images/icon-minus.svg"
-        element.alt = "Icon for opened accordion"
-    } else if (!isItemActive){
-        element.src = "./assets/images/icon-plus.svg"
-        element.alt = "Icon for closed accordion"
-    }
-  });
+  if (!isExpanded) {
+    element.src = "./assets/images/icon-minus.svg"
+    element.alt = "Icon for opened accordion"
+  } else if (isExpanded){
+    element.src = "./assets/images/icon-plus.svg"
+    element.alt = "Icon for closed accordion"
 }
+}
+
+function init() {
+  if (!disclosures.length) {
+    return
+  }
+  
+  disclosures.forEach(component => {
+    component.dataset.state = 'ready';
+    
+    const disclosureButtons = component.querySelectorAll('.js-disclosure-btn');
+    
+    disclosureButtons.forEach(btn => {
+      btn.removeAttribute('title');
+      btn.removeAttribute('disabled');
+
+      btn.addEventListener('click', toggleDisclosure);
+    });
+  }); 
+  
+  
+}
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    init();
+  });
